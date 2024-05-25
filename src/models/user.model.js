a1443c456096bda4e90ffedd21d969a7fa0e5864
Ayush Maurya <amaurya5556@gmail.com>
2024-05-25 21:31:48 +0530

    Add access and refresh token generation methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
@@ -32,5 +34,57 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// schema has methods object as property to which we can add
+// our own methods as required
+// this inside the methods has access to data in the schema
+
+// create custom methods
+// while creating new methods never use arrow functions
+// because they once and forever bound to the outer context at the
+// point of creation in this case to the global object
+// and invoking it will perform no desired effect
+
+// we are not using async here since its genrally very fast
+// but we could have used
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
